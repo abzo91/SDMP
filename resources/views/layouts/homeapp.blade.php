85a9d12eff686a35d23ad21321275e602a2badb6
NWNJ6927 <abdelrahman.essameldin@orange.com>
2018-11-01 14:47:36 +0200

    Footer modification

--- resources/views/layouts/homeapp.blade.php
+++ resources/views/layouts/homeapp.blade.php
@@ -33,7 +33,9 @@
        .navbaricons{
 			  font-size: 150%;
 		  }
-	
+      .notificationsize{
+            font-size: 200px;
+          }
         .main-sidebar {
             z-index: 1;
         }
@@ -286,56 +288,49 @@ input[type=submit] {
 			  </li>
         <!-- /.sidebar -->
 			</ul>
-			  </section>
-      </aside>
-      @yield('content')
-      <!-- jQuery 2.1.3 -->
-    <script src="{{ asset('homepublic/plugins/jQuery/jQuery-2.1.3.min.js')}}"></script>
-    <!-- Bootstrap 3.3.2 JS -->
-    <script src="{{ asset('homepublic/bootstrap/js/bootstrap.min.js')}}" type="text/javascript"></script>
-    <!-- FastClick -->
-    <script src="{{ asset('homepublic/plugins/fastclick/fastclick.min.js')}}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
-    <script src="{{ asset('homepublic/dist/js/app.min.js')}}" type="text/javascript"></script>
-    <!-- Sparkline -->
-    <script src="{{ asset('homepublic/plugins/sparkline/jquery.sparkline.min.js')}}" type="text/javascript"></script>
-    <!-- jvectormap -->
-    <script src="{{ asset('homepublic/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}" type="text/javascript"></script>
-    <script src="{{ asset('homepublic/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}" type="text/javascript"></script>
-    <!-- daterangepicker -->
-    <script src="{{ asset('homepublic/plugins/daterangepicker/daterangepicker.js')}}" type="text/javascript"></script>
-    <!-- datepicker -->
-    <script src="{{ asset('homepublic/plugins/datepicker/bootstrap-datepicker.js')}}" type="text/javascript"></script>
-    <!-- iCheck -->
-    <script src="{{ asset('homepublic/plugins/iCheck/icheck.min.js')}}" type="text/javascript"></script>
-    <!-- SlimScroll 1.3.0 -->
-    <script src="{{ asset('homepublic/plugins/slimScroll/jquery.slimscroll.min.js')}}" type="text/javascript"></script>
-    <!-- ChartJS 1.0.1 -->
-    <script src="{{ asset('homepublic/plugins/chartjs/Chart.min.j')}}s" type="text/javascript"></script>
+      </section>
+        </aside>
+        @yield('content')
+        <!-- jQuery 2.1.3 -->
+        <script src="{{ asset('homepublic/plugins/jQuery/jQuery-2.1.3.min.js')}}"></script>
+        <!-- Bootstrap 3.3.2 JS -->
+        <script src="{{ asset('homepublic/bootstrap/js/bootstrap.min.js')}}" type="text/javascript"></script>
+        <!-- FastClick -->
+        <script src="{{ asset('homepublic/plugins/fastclick/fastclick.min.js')}}"></script>
+        <!-- AdminLTE App -->
+        <script src="{{ asset('js/app.js') }}" charset="utf-8"></script>
+        <script src="{{ asset('homepublic/dist/js/app.min.js')}}" type="text/javascript"></script>
+        <!-- Sparkline -->
+        <script src="{{ asset('homepublic/plugins/sparkline/jquery.sparkline.min.js')}}" type="text/javascript"></script>
+        <!-- jvectormap -->
+        <script src="{{ asset('homepublic/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('homepublic/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}" type="text/javascript"></script>
+        <!-- daterangepicker -->
+        <script src="{{ asset('homepublic/plugins/daterangepicker/daterangepicker.js')}}" type="text/javascript"></script>
+        <!-- datepicker -->
+        <script src="{{ asset('homepublic/plugins/datepicker/bootstrap-datepicker.js')}}" type="text/javascript"></script>
+        <!-- iCheck -->
+        <script src="{{ asset('homepublic/plugins/iCheck/icheck.min.js')}}" type="text/javascript"></script>
+        <!-- SlimScroll 1.3.0 -->
+        <script src="{{ asset('homepublic/plugins/slimScroll/jquery.slimscroll.min.js')}}" type="text/javascript"></script>
+        <!-- ChartJS 1.0.1 -->
+        <script src="{{ asset('homepublic/plugins/chartjs/Chart.min.j')}}s" type="text/javascript"></script>
+        <!-- AdminLTE for demo purposes -->
+        <div>
+            <footer class="main-footer" style="background-color: black !important;">
+                <div>
+                    <a href="#" class="policy">Security Policy</a>
+                    <a href="#" class="contactus">Contact Us </a>
+                    <a href="#" class="support">Support</a>
+                    <a href="#" class="support">About</a>
+                </div>
+                <div class="copyright">
+                    <p style="color: white">©Developed by IT PA & UE Team </p>
+                </div>
+            </footer>
+        </div>
+    </div>
+    @yield('contenttwo')
+</body>
 
-    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="{{ asset('homepublic/dist/js/pages/dashboard2.js')}}" type="text/javascript"></script>
-
-    <!-- AdminLTE for demo purposes -->
-	  </div>
-  </body>
-     
-
-   	  <footer class="footer">
-  &nbsp;
-  <div class="footericons">	
-    <a href="#" class="policy">Security Policy</a>
-    <a href="#" class="contactus">Contact Us </a>
-    <a href="#" class="support">Support</a>
-	  <a href="#" class="support">About</a>
-    
-  </div>
-		<div class="copyright">
-    <p style="color: white"> &nbsp; &nbsp; ©Developed by IT PA & UE Team </p>
-  
-		</div>
-		
-</footer>
-  
 </html>
